6da404705d4af5f3044c5274a5259431f99c6924
vannuanhthu <103262482+vannuanhthu@users.noreply.github.com>
2022-05-25 10:24:49 +0700

    Revert "feat: update NodeMCU firmware"
    This reverts commit 90479f94aa836dae3e923458b67b8ac7e2204caf.

--- software/NodeMCU/NodeMCU.ino
+++ software/NodeMCU/NodeMCU.ino
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 5ffacb0 (uart_Senddata)
 /*************************************************************
   Download latest Blynk library here:
     https://github.com/blynkkk/blynk-library/releases/latest
@@ -50,7 +46,7 @@
 
 #include <ESP8266WiFi.h>
 //#include <Blynk.h>
-#include <BlynkExampleEsp8266.h>
+#include <BlynkSimpleEsp8266.h> 
 //#include <BlynkEdgent.h>
 
 // You should get Auth Token in the Blynk App.
@@ -59,22 +55,16 @@ char auth[] = "QmZ0ozni5rHRDzknAr6cT6kGkhPQRmm8";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-<<<<<<< HEAD
 char ssid[] = "HCMUS-VLDT-SV";
 char pass[] = "svvldt38300595";
-=======
-char ssid[] = "Truc Mai 3";
-char pass[] = "10031953456";
-int Temp;
->>>>>>> parent of 5ffacb0 (uart_Senddata)
 int aboveThres;
 int belowThres;
 uint8_t u8aboveThres, u8belowThres;
-
+int Hot, Cool, Cold, Temp;
 void setup()
 {
   // Debug console
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   Blynk.begin(auth, ssid, pass);
   // You can also specify server:
@@ -93,7 +83,6 @@ BLYNK_WRITE(V2) {
 void loop()
 {
   Blynk.run();
-<<<<<<< HEAD
 
  //Transmit Threshold from nodeMCU to ATMega
   Serial.println(aboveThres);
@@ -112,15 +101,6 @@ Temp = 20;
   
   //Compare Curren Tempurature with Threshold
   if (Temp > aboveThres){
-=======
-  Serial.println(aboveThres | 0x00000100);
-  Serial.println(belowThres | 0x00000100);
-  if (Serial.available()) {
-    Temp = Serial.read();
-  }
-  Blynk.virtualWrite(V0, Temp); // Truyen len Blynk
-  if (Temp >= aboveThres){
->>>>>>> parent of 5ffacb0 (uart_Senddata)
     Hot = 1;
     Cold = 0;
     Cool = 0;
@@ -128,15 +108,7 @@ Temp = 20;
     Blynk.virtualWrite(V5, Cold);
     Blynk.virtualWrite(V6, Cool);
   }
-  if (belowThres < Temp &&  Temp < aboveThres) {
-    Hot = 0;
-    Cold = 0;
-    Cool = 1;
-    Blynk.virtualWrite(V4, Hot);
-    Blynk.virtualWrite(V5, Cold);
-    Blynk.virtualWrite(V6, Cool);
-  }
-    if(Temp <= belowThres){
+   if (belowThres < Temp &&  Temp < aboveThres) {
     Hot = 0;
     Cold = 0;
     Cool = 1;
@@ -145,7 +117,6 @@ Temp = 20;
     Blynk.virtualWrite(V6, Cool);
   }
   
-<<<<<<< HEAD
 
     if(Temp < belowThres){
     Hot = 0;
@@ -156,6 +127,4 @@ Temp = 20;
     Blynk.virtualWrite(V6, Cool);
   }
    
-=======
->>>>>>> parent of 5ffacb0 (uart_Senddata)
 }
