ac3c31a8fc8a76d0834aef18f03e89ab8f440d10
vannuanhthu <103262482+vannuanhthu@users.noreply.github.com>
2022-06-03 10:35:08 +0700

    nodeMCU

--- software/NodeMCU/NodeMCU.ino
+++ software/NodeMCU/NodeMCU.ino
@@ -1,58 +1,17 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-  This example runs directly on NodeMCU.
-
-  Note: This requires ESP8266 support package:
-    https://github.com/esp8266/Arduino
-
-  Please be sure to select the right NodeMCU module
-  in the Tools -> Board menu!
-
-  For advanced settings please follow ESP examples :
-   - ESP8266_Standalone_Manual_IP.ino
-   - ESP8266_Standalone_SmartConfig.ino
-   - ESP8266_Standalone_SSL.ino
-
-  Change WiFi ssid, pass, and Blynk auth token to run :)
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
 /* Comment this out to disable prints and save space */
 // #define BLYNK_PRINT Serial
 
-/* Fill-in your Template ID (only if using Blynk.Cloud) */
+/* Fill-in your Template ID */
 //#define BLYNK_TEMPLATE_ID   "YourTemplateID"
 #define BLYNK_TEMPLATE_ID "TMPLddsQgzgp"
 #define BLYNK_DEVICE_NAME "Temperature Control"
 #define BLYNK_AUTH_TOKEN "QmZ0ozni5rHRDzknAr6cT6kGkhPQRmm8"
 
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
 char auth[] = "QmZ0ozni5rHRDzknAr6cT6kGkhPQRmm8";
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "HCMUS-VLDT-SV";
 char pass[] = "svvldt38300595";
 
@@ -63,7 +22,6 @@ int Hot = 0;
 int Cool = 0;
 int Cold = 0;
 
-
 void setup()
 {
   // Debug console
@@ -73,13 +31,14 @@ void setup()
   Blynk.syncVirtual(V1);
   Blynk.syncVirtual(V2);
 }
-BLYNK_WRITE(V1) {
+BLYNK_WRITE(V1) 
+{
   thershold_buffer[0] = param.asInt(); //Recieve aboveThreshold from Blynk
 }
-BLYNK_WRITE(V2) {
+BLYNK_WRITE(V2) 
+{
   thershold_buffer[1] = param.asInt(); //Recieve belowThreshold from Blynk
 }
-
 
 void loop()
 {
@@ -125,5 +84,4 @@ void loop()
     Blynk.virtualWrite(V5, Cold);
     Blynk.virtualWrite(V6, Cool);
   }
-
 }
